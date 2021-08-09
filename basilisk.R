eb8643db8dffec7e30330c87e47106891bffb3fe
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-08-09 17:38:24 -0500

    delet unnecessary files

--- basilisk.R
+++
@@ -1,8 +0,0 @@
-openreviewEnvironment <- basilisk::BasiliskEnvironment(
-    envname = "openreviewEnvironment",
-    pkgname = "openreviewr",
-    packages = character(0),
-    pip = c("openreview-py==1.0.22")
-)
-
-
