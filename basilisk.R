a0c858f36826bd01bbd8e21e07e74d53b505a398
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-08-09 17:06:14 -0500

    first commit

---
+++ basilisk.R
@@ -0,0 +1,8 @@
+openreviewEnvironment <- basilisk::BasiliskEnvironment(
+    envname = "openreviewEnvironment",
+    pkgname = "openreviewr",
+    packages = character(0),
+    pip = c("openreview-py==1.0.22")
+)
+
+
